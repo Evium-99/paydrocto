aef7e49d8878b8d39dffdb23dad971bc14fc1918
AntonDaAnt <57422635+Suave101@users.noreply.github.com>
2025-10-26 15:47:09 -0500

    subsystems gooood no auto :(
    me me need auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroAuto.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.Shooter;
+
+@Configurable
+@Autonomous
+public class pedroAuto extends OpMode {
+    private Follower follower;
+    public static Pose startingPose; //See ExampleAuto to understand how to use this
+    private TelemetryManager telemetryM;
+    private boolean slowMode = false;
+    private double slowModeMultiplier = 0.5;
+    private Shooter shooter;
+    private Intake intake;
+    public static double rpm = 10;
+
+    @Override
+    public void init() {
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(startingPose == null ? new Pose() : startingPose);
+        follower.update();
+        telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
+        shooter = new Shooter(hardwareMap, "leftShooter", "rightShooter", telemetry);
+        intake = new Intake(hardwareMap, "lpull", "rpull", telemetry);
+    }
+
+    @Override
+    public void start() {
+        //The parameter controls whether the Follower should use break mode on the motors (using it is recommended).
+        //In order to use float mode, add .useBrakeModeInTeleOp(true); to your Drivetrain Constants in Constant.java (for Mecanum)
+        //If you don't pass anything in, it uses the default (false)
+        follower.start
+    }
+
+    @Override
+    public void loop() {
+        //Call this once per loop
+        follower.update();
+        telemetryM.update();
+        shooter.periodic();
+        intake.periodic();
+        
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroTeleop.java
@@ -10,7 +10,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.subsystems.Shooter;
-import org.firstinspires.ftc.teamcode.subsystems.Intake;
 
 @Configurable
 @TeleOp
@@ -18,8 +17,6 @@ public class pedroTeleop extends OpMode {
     private Follower follower;
     public static Pose startingPose; //See ExampleAuto to understand how to use this
     private TelemetryManager telemetryM;
-    private boolean slowMode = false;
-    private double slowModeMultiplier = 0.5;
     private Shooter shooter;
     private Intake intake;
     public static double rpm = 10;
@@ -31,6 +28,7 @@ public class pedroTeleop extends OpMode {
         follower.update();
         telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
         shooter = new Shooter(hardwareMap, "leftShooter", "rightShooter", telemetry);
+        intake = new Intake(hardwareMap, "lpull", "rpull", telemetry);
     }
 
     @Override
@@ -47,49 +45,70 @@ public class pedroTeleop extends OpMode {
         follower.update();
         telemetryM.update();
         shooter.periodic();
+        intake.periodic();
 
-        if (gamepad2.a) {
-            shooter.setVelocity(rpm);
-        } else {
+        //Gamepad 2 controls:
+        if (gamepad2.right_trigger > 0.05) {
+            while (gamepad2.right_trigger > 0.05) {
+                shooter.setVelocity(rpm);
+                intake.servospin(1);
+            }
             shooter.setVelocity(0);
-        }
-        if (gamepad2.x) {
-            intake.servospin(1);
-        } else {
             intake.servospin(0);
         }
+        else if (gamepad2.left_trigger > 0.05) {
+            while (gamepad2.left_trigger > 0.05) {
+                intake.servospin(-1);
+            }
+            intake.servospin(0);
+        }
+        else if (gamepad2.a) {
+            while (gamepad2.a) {
+                shooter.setVelocity(-rpm / 4);
+                intake.servospin(1);
+            }
+            shooter.setVelocity(0);
+            intake.servospin(0);
+        }
+        if (gamepad2.left_stick_y < -0.1) {
+            while (gamepad2.left_stick_y < -0.1) {
+                intake.servospin(-1);
+            }
+            intake.servospin(0);
+        }
+        if (gamepad2.left_stick_y > 0.1) {
+            while (gamepad2.left_stick_y > 0.1) {
+                intake.servospin(1);
+            }
+            intake.servospin(0);
+        }
+        if (gamepad2.right_stick_y < -0.1) {
+            while (gamepad2.right_stick_y < -0.1) {
+                shooter.setVelocity(-rpm);
+            }
+            shooter.setVelocity(0);
+        }
+        if (gamepad2.right_stick_y > 0.1) {
+            while (gamepad2.right_stick_y > 0.1) {
+                shooter.setVelocity(rpm);
+            }
+            shooter.setVelocity(0);
+        }
 
 
-
-        if (!slowMode) follower.setTeleOpDrive(
-                -gamepad1.left_stick_y,
-                -gamepad1.left_stick_x,
-                -gamepad1.right_stick_x,
-                true // Robot Centric
-        );
-
-            //This is how it looks with slowMode on
-        else follower.setTeleOpDrive(
+        //Gamepad 1 controls
+        double slowModeMultiplier = 0.25;
+        if (gamepad1.right_trigger > 0.05) follower.setTeleOpDrive(
                 -gamepad1.left_stick_y * slowModeMultiplier,
                 -gamepad1.left_stick_x * slowModeMultiplier,
                 -gamepad1.right_stick_x * slowModeMultiplier,
                 true // Robot Centric
         );
-
-        //Slow Mode
-        if (gamepad1.rightBumperWasPressed()) {
-            slowMode = !slowMode;
-        }
-
-        //Optional way to change slow mode strength
-        if (gamepad1.xWasPressed()) {
-            slowModeMultiplier += 0.25;
-        }
-
-        //Optional way to change slow mode strength
-        if (gamepad2.yWasPressed()) {
-            slowModeMultiplier -= 0.25;
-        }
-
+        else follower.setTeleOpDrive(
+                -gamepad1.left_stick_y,
+                -gamepad1.left_stick_x,
+                -gamepad1.right_stick_x,
+                true // Robot Centric
+        );
     }
 }
